ccabfce5c182aa145e66c689fc1cad2bf53aae9c
theRealRobG <robertnw5@gmail.com>
2023-12-10 15:39:03 -0500

    Added operation queue concept

---
+++ src/utils/async-operation-queue/index.ts
@@ -0,0 +1,70 @@
+import EventEmitter from '../event-emitter';
+
+interface AsyncOperationEventMap {
+  completed: void;
+}
+
+export class AsyncOperation<
+  T extends string | number,
+> extends EventEmitter<AsyncOperationEventMap> {
+  public isCancelled = false;
+
+  constructor(
+    public type: T,
+    private action: (op: AsyncOperation<T>) => Promise<void>
+  ) {
+    super({completed: new Set()});
+  }
+
+  public run() {
+    this.action(this).finally(() => this.notifyEvent('completed'));
+  }
+}
+
+export default class AsyncOperationQueue<T extends string | number> {
+  private operationQueue: AsyncOperation<T>[] = [];
+
+  private onOperationCompleted = () => {
+    // Remove completed operation
+    const completedOperation = this.operationQueue.shift();
+    completedOperation?.removeEventListener(
+      'completed',
+      this.onOperationCompleted
+    );
+    // Start next operation if it exists
+    if (this.operationQueue.length > 0) {
+      this.startOperation(this.operationQueue[0]);
+    }
+  };
+
+  public addOperation(operation: AsyncOperation<T>) {
+    this.operationQueue.push(operation);
+    if (this.operationQueue.length === 1) {
+      this.startOperation(operation);
+    }
+  }
+
+  public removeOperationAtIndex(index: number) {
+    const operation = this.operationQueue[index];
+    if (operation) {
+      operation.isCancelled = true;
+    }
+    if (index === 0) {
+      this.onOperationCompleted();
+    } else {
+      this.operationQueue.splice(index, 1);
+    }
+  }
+
+  public removeOperation(operation: AsyncOperation<T>) {
+    const index = this.operationQueue.findIndex(op => op === operation);
+    if (index !== -1) {
+      this.removeOperationAtIndex(index);
+    }
+  }
+
+  private startOperation(operation: AsyncOperation<T>) {
+    operation.addEventListener('completed', this.onOperationCompleted);
+    operation.run();
+  }
+}
--- src/utils/event-emitter/index.ts
+++ src/utils/event-emitter/index.ts
@@ -15,10 +15,9 @@ export default class EventEmitter<EventMap> {
    * The initial callbacks should all just be empty sets (`new Set()`).
    */
   constructor(
-    protected eventCallbacks: Record<
-      keyof EventMap,
-      Set<(ev: EventMap[keyof EventMap]) => void>
-    >
+    protected eventCallbacks: {
+      [T in keyof EventMap]: Set<(event: EventMap[T]) => void>;
+    }
   ) {}
 
   /**
@@ -27,9 +26,9 @@ export default class EventEmitter<EventMap> {
    * @param type The event name to add a listener for.
    * @param listener A callback to handle when the event is fired.
    */
-  public addEventListener(
-    type: keyof EventMap,
-    listener: (ev: EventMap[keyof EventMap]) => unknown
+  public addEventListener<T extends keyof EventMap>(
+    type: T,
+    listener: (ev: EventMap[T]) => unknown
   ): void {
     this.eventCallbacks[type].add(listener);
   }
@@ -40,11 +39,21 @@ export default class EventEmitter<EventMap> {
    * @param type The event name to remove the listener from.
    * @param listener The listener to remove.
    */
-  public removeEventListener(
-    type: keyof EventMap,
-    listener: (ev: EventMap[keyof EventMap]) => unknown
+  public removeEventListener<T extends keyof EventMap>(
+    type: T,
+    listener: (ev: EventMap[T]) => unknown
   ): void {
     this.eventCallbacks[type].delete(listener);
+  }
+
+  public waitForEvent<T extends keyof EventMap>(type: T): Promise<EventMap[T]> {
+    return new Promise(resolve => {
+      const callback = (event: EventMap[T]) => {
+        this.removeEventListener(type, callback);
+        resolve(event);
+      };
+      this.addEventListener(type, callback);
+    });
   }
 
   /**
